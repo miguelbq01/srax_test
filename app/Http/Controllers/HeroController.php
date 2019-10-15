e95e83506b5c28c6d7c01a9729cb8675503c6e3e
Mike Bolaños <miguelbq88@gmail.com>
2019-10-14 21:16:33 -0700

    Cambios heroes

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -10,18 +10,31 @@ use App\HeroWeapon;
 
 class HeroController extends Controller
 {
+    // Mostrar listado de heroes disponibles
     public function index(){
         return Hero::all();
     }
 
+    // Mostrar un heroe especifico por ID
     public function show($id){
         return Hero::find($id);
     }
 
+    // Crear un nuevo heroe
     public function create(Request $request){
-        return Hero::create($request->all());
+
+        $validatesNames = $this->validateNames($request->all());
+
+        if ($validatesNames === true) {
+            return Hero::create($request->all());
+        }else {
+            return response()-> json([
+                "error" => $validatesNames
+            ]);
+        }
     }
 
+    // Editar un heroe por su ID
     public function update(Request $request, $id){
         $hero = Hero::findOrFail($id);
         $hero->update($request->all());
@@ -29,6 +42,7 @@ class HeroController extends Controller
         return $hero;
     }
 
+    // Eliminar un heroe por su ID
     public function delete(Request $request, $id){
         $hero = Hero::findOrFail($id);
         $hero->delete();
@@ -36,6 +50,22 @@ class HeroController extends Controller
         return 204;
     }
 
+    // Listado de todas las Razas de Heroes existentes
+    public function showRaces(){
+        return HeroRace::all();
+    }
+
+    // Listado de todas las clases de Heroes existentes
+    public function showClasses(){
+        return HeroClass::all();
+    }
+
+    // Listado de todas las armas de Heroes existenes
+    public function showWeapons(){
+        return HeroWeapon::all();
+    }
+
+    // Mostrar cuantos heroes existen
     public function maxNumberHeroes(){
         $count = Hero::all()->count();
         return response()-> json([
@@ -43,6 +73,7 @@ class HeroController extends Controller
         ]);
     }
 
+    // Mostrar cual es la Raza mas popular
     public function popularRace(){
         $popular = Hero::selectRaw('count(race_id) as race, race_id')
                 ->orderBy('race', 'desc')
@@ -57,6 +88,7 @@ class HeroController extends Controller
         ]);
     }
 
+    // Mostrar cual es la Clase mas popular
     public function popularClass(){
         $popular = Hero::selectRaw('count(class_id) as class, class_id')
                 ->orderBy('class', 'desc')
@@ -71,6 +103,7 @@ class HeroController extends Controller
         ]);
     }
 
+    // Mostrar cual es la Arma mas popular
     public function popularWeapon(){
         $popular = Hero::selectRaw('count(weapon_id) as weapon, weapon_id')
                 ->orderBy('weapon', 'desc')
@@ -83,5 +116,50 @@ class HeroController extends Controller
         return response()-> json([
             "popular_hero_weapon" => $popular_weapon['name']
         ]);
+    }
+
+    // Validaciones a los nombres de Heroe
+    public function validateNames($request){
+        // Verificamos si la clase del heroe es un Half Orc o Dragonborn
+        // Si es asi, regresar error si tiene Last Name
+        $half_orc_race = HeroRace::where('name', '=', "Half-orc")->first();
+        $dragonborn_race = HeroRace::where('name', '=', "Dragonborn")->first();
+        
+        if ($request['race_id'] === $half_orc_race['id'] || $request['race_id'] === $dragonborn_race['id'] ) {
+            if ($request['last_name'] !== "") {
+                return "En las clases Half-orc y Dragonborn no deben tener Last Name";
+            };
+        };
+        
+        // Si es Elf, Last Name debe ser el First Name invertido
+        $elf_race = HeroRace::where('name', '=', "Elf")->first();
+        
+        if ($request['race_id'] === $elf_race['id'] ) {
+            if ($request['last_name'] !== strrev($request['first_name'])) {
+                return "En la clase Elf, el Last Name debe ser el First Name invertido.";
+            };
+        };
+        
+        // Si es Dwarf, First y Last Name deben de contener al menos una 'R' o 'H'
+        $dwarf_race = HeroRace::where('name', '=', "Dwarf")->first();
+
+        if ($request['race_id'] === $dwarf_race['id'] ) {
+            $first_name_r = strpos($request['first_name'], "R");
+            $first_name_h = strpos($request['first_name'], "H");
+
+            $last_name_r = strpos($request['last_name'], "R");
+            $last_name_h = strpos($request['last_name'], "H");
+
+            if ($first_name_r !== false || $first_name_h !== false) {
+                if ($last_name_r === false && $last_name_h === false) {
+                    return "En la clase Dwarf, el Last Name debe contener una 'R' o una 'H'.";
+                }
+            }else {
+                return "En la clase Dwarf, el First Name debe contener una 'R' o una 'H'.";
+            };
+        };
+
+        return true;
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,11 @@ use Illuminate\Http\Request;
 // Heroes Routes
 Route::get('heroes', 'HeroController@index');
 Route::get('heroes/{id}', 'HeroController@show');
+
+Route::get('show_hero_classes', 'HeroController@showClasses');
+Route::get('show_hero_races', 'HeroController@showRaces');
+Route::get('show_hero_weapons', 'HeroController@showWeapons');
+
 Route::get('max_number_heroes', 'HeroController@maxNumberHeroes');
 Route::get('popular_hero_race', 'HeroController@popularRace');
 Route::get('popular_hero_class', 'HeroController@popularClass');
